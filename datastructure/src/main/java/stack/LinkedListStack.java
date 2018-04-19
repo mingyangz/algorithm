3ff95701df3942f5de42b034ea854b4bed3b01b4
Mingyang Zhou <mingyangzhou186@gmail.com>
2018-04-18 18:35:07 -0700

    LinkedList stack of integer

---
+++ datastructure/src/main/java/stack/LinkedListStack.java
@@ -0,0 +1,59 @@
+package stack;
+
+/**
+ * LinkedList implementation of Stack of Integers.
+ */
+public class LinkedListStack {
+    /**
+     * LinkedList Node.
+     */
+    private class Node {
+        int val;
+        Node next;
+    }
+
+    private Node head;
+    private int size;
+
+    public LinkedListStack() {
+        head = null;
+    }
+
+    /**
+     * Push an integer to the stack.
+     * @param x int to be pushed
+     */
+    public void push(int x) {
+        Node newHead = new Node();
+        newHead.val = x;
+        newHead.next = head;
+        head = newHead;
+        size++;
+    }
+
+    /**
+     * Retrieve and remove the first element of the stack.
+     * @return first element of the stack
+     */
+    public int pop() {
+        int top = head.val;
+        head = head.next;
+        return top;
+    }
+
+    /**
+     * Size of the stack.
+     * @return size of the stack
+     */
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Return true if the stack is empty.
+     * @return true if the stack is empty
+     */
+    public boolean isEmpty() {
+        return head == null;
+    }
+}
