f78b53a0afbfcc0a86dc6639af6e1f950f9f2267
Mingyang Zhou <mingyangzhou186@gmail.com>
2018-04-23 22:21:51 -0700

    resizing array deque

---
+++ datastructure/src/main/java/queue/ResizedArrayDeque.java
@@ -0,0 +1,82 @@
+package queue;
+
+public class ResizedArrayDeque {
+    private int[] deque;
+    private int size;
+    private int head;
+    private int tail;
+
+    // construct an empty deque
+    public ResizedArrayDeque() {
+        deque = new int[2];
+        size = 0;
+        head = 0;
+        tail = 0;
+    }
+
+    // is the deque empty?
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // return the number of items on the deque
+    public int size() {
+        return size;
+    }
+
+    // add the item to the front
+    public void addFirst(int x) {
+        if (size == deque.length) {
+            resize(deque.length * 2);
+        }
+        if (head == -1) {
+            head = deque.length - 1;
+        }
+        deque[head--] = x;
+        size++;
+    }
+
+    // add the item to the end
+    public void addLast(int x) {
+        if (size == deque.length) {
+            resize(deque.length * 2);
+        }
+        if (tail == deque.length) {
+            tail = 0;
+        }
+        deque[tail++] = x;
+        size++;
+    }
+
+    // remove and return the item from the front
+    public int removeFirst() {
+        if (size > 0 && size == deque.length / 4) {
+            resize(deque.length / 2);
+        }
+        if (head == deque.length - 1) {
+            head = -1;
+        }
+        return deque[++head];
+    }
+
+    // remove and return the item from the end
+    public int removeLast() {
+        if (size > 0 && size == deque.length / 4) {
+            resize(deque.length / 2);
+        }
+        if (tail == -1) {
+            tail = deque.length;
+        }
+        return deque[--tail];
+    }
+
+    private void resize(int capacity) {
+        int[] copy = new int[capacity];
+        for (int i = 0; i < size; i++) {
+            copy[i] = deque[(head + i + 1) % deque.length];
+        }
+        deque = copy;
+        head = -1;
+        tail = size;
+    }
+}
---
+++ datastructure/src/test/java/queue/TestResizedArrayDeque.java
@@ -0,0 +1,24 @@
+package queue;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestResizedArrayDeque {
+    @Test
+    public void test1() {
+        ResizedArrayDeque deque = new ResizedArrayDeque();
+        deque.addFirst(1);
+        deque.addFirst(2);
+        deque.addLast(3);
+        deque.addLast(4);
+        deque.addFirst(5);
+        deque.addLast(6);
+        assertEquals(5, deque.removeFirst());
+        assertEquals(2, deque.removeFirst());
+        assertEquals(1, deque.removeFirst());
+        assertEquals(3, deque.removeFirst());
+        assertEquals(6, deque.removeLast());
+        assertEquals(4, deque.removeLast());
+    }
+}
