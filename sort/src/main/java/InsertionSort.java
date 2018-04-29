9decec772fce3eea198df3853bd881449573bb4b
Mingyang Zhou <mingyangzhou186@gmail.com>
2018-04-29 12:01:31 -0700

    fix bug in insertion sort

--- sort/src/main/java/InsertionSort.java
+++ sort/src/main/java/InsertionSort.java
@@ -8,10 +8,10 @@ public class InsertionSort {
             return;
         }
         for (int i = 1; i < nums.length; i++) {
-            for (int j = i; j > 0; j--) {
-                if (nums[j] < nums[j - 1]) {
-                    SortUtils.swap(nums, j - 1, j);
-                }
+            int j = i;
+            while (j > 0 && nums[j] < nums[j - 1]) {
+                SortUtils.swap(nums, j - 1, j);
+                j--;
             }
         }
     }
